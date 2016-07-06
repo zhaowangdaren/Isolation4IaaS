8d6e3299e381a4f8e6cd378c8855df18f550b7f4
zhaowangdaren <Mini-zy@ustcdeMac-mini.local>
2016-07-06 20:28:29 +0800

    成功实现setInfo事件

--- src/main/java/cn/edu/ustc/views/AppController.java
+++ src/main/java/cn/edu/ustc/views/AppController.java
@@ -1,9 +1,16 @@
 package cn.edu.ustc.views;
 
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
 import org.json.JSONObject;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
 
 import cn.edu.ustc.CloudInfo;
@@ -16,14 +23,43 @@ public class AppController {
 		return "setView";
 	}
 	
-	@RequestMapping("/setInfo")
-	public String setInfo(String computeEP, String networkEP, String adminUsername, 
-			String adminPassword, String mqURL){
-		if(computeEP != null)CloudInfo.COMPUTE_EP = computeEP;
-		if(networkEP != null)CloudInfo.NETWORK_EP = networkEP;
-		if(adminUsername != null) CloudInfo.ADMIN_USERNAME = adminUsername;
-		if(adminPassword != null)CloudInfo.ADMIN_PASSWORD = adminPassword;
-		if(mqURL != null) CloudInfo.MQ_URL = mqURL;
+//	@RequestMapping("/setInfo")
+//	public String setInfo(String computeEP, String networkEP, String adminUsername, 
+//			String adminPassword, String mqURL){
+//		if(computeEP != null)CloudInfo.COMPUTE_EP = computeEP;
+//		if(networkEP != null)CloudInfo.NETWORK_EP = networkEP;
+//		if(adminUsername != null) CloudInfo.ADMIN_USERNAME = adminUsername;
+//		if(adminPassword != null)CloudInfo.ADMIN_PASSWORD = adminPassword;
+//		if(mqURL != null) CloudInfo.MQ_URL = mqURL;
+//		
+//		JSONObject result = new JSONObject();
+//		result.append("computeEP", CloudInfo.COMPUTE_EP);
+//		result.append("networkEP", CloudInfo.NETWORK_EP);
+//		result.append("adminUsername", CloudInfo.ADMIN_USERNAME);
+//		result.append("adminPassword", CloudInfo.ADMIN_PASSWORD);
+//		result.append("mqURL", CloudInfo.MQ_URL);
+//		
+//		return result.toString();
+//	}
+	
+	@RequestMapping("/test")
+	public ModelAndView testView(){
+		ModelAndView result = new ModelAndView("test");
+		return result;
+	}
+	
+	@RequestMapping(value = "/setInfo", method=RequestMethod.POST)
+	public void setInfo(HttpServletRequest request, HttpServletResponse response){
+		String computeEP = request.getParameter("computeEP");
+		String networkEP = request.getParameter("networkEP");
+		String adminUsername = request.getParameter("adminUsername");
+		String adminPassword = request.getParameter("adminPassword");
+		String mqURL = request.getParameter("mqURL");
+		if(computeEP != null && computeEP.length() > 0)CloudInfo.COMPUTE_EP = computeEP;
+		if(networkEP != null && networkEP.length() > 0)CloudInfo.NETWORK_EP = networkEP;
+		if(adminUsername != null && adminUsername.length() > 0) CloudInfo.ADMIN_USERNAME = adminUsername;
+		if(adminPassword != null && adminPassword.length() > 0)CloudInfo.ADMIN_PASSWORD = adminPassword;
+		if(mqURL != null && mqURL.length() > 0) CloudInfo.MQ_URL = mqURL;
 		
 		JSONObject result = new JSONObject();
 		result.append("computeEP", CloudInfo.COMPUTE_EP);
@@ -31,13 +67,12 @@ public class AppController {
 		result.append("adminUsername", CloudInfo.ADMIN_USERNAME);
 		result.append("adminPassword", CloudInfo.ADMIN_PASSWORD);
 		result.append("mqURL", CloudInfo.MQ_URL);
-		
-		return result.toString();
+		try {
+			response.getWriter().write(result.toString());
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 	}
 	
-	@RequestMapping("/test")
-	public ModelAndView testView(){
-		ModelAndView result = new ModelAndView("test");
-		return result;
-	}
 }
--- src/main/webapp/WEB-INF/views/setView.jsp
+++ src/main/webapp/WEB-INF/views/setView.jsp
@@ -4,41 +4,42 @@
 <head>
 <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
 <title>设置基本信息</title>
-<script type="text/javascript" src="/resources/js/jquery-3.0.0.min.js"></script>
-<script >
-$(document).ready(function(){
-  $("p").click(function(){
-    $(this).hide();
-  });
-});
+<script type="text/javascript" src="resources/js/jquery-3.0.0.min.js"></script>
+<script type="text/javascript">
+function setInfo(){
+	$.post("setInfo",
+	{
+		computeEP:$("#computeEP").val(),
+		networkEP:$("#networkEP").val(),
+		adminUsername:$("#adminUsername").val(),
+		adminPassword:$("#adminPassword").val(),
+		mqURL:$("#mqURL").val()
+	},
+	function(data, status){
+		alert("Data: "+data+"\nStatus: "+status);
+	});
+}
 </script>
+ 
 <!-- menu -->
-
-</head>
-<body>
 <ul>
 <li><a href="#">设置</a></li>
 </ul>
+</head>
+<body>
+
 <div>
 OpenStack Compute Endpoint:${computeEP}
-<p><input type="text" name="computeEP" size="100"/></p>
+<p><input type="text" id="computeEP" size="100"/></p>
 OpenStack Network Endpoint:${networkEP}
-<p><input type="text" name="networkEP" size="100"/></p>
+<p><input type="text" id="networkEP" size="100"/></p>
 OpenStack Admin Username:${adminUsername}
-<p><input type="text" name="adminUsername" /></p>
+<p><input type="text" id="adminUsername" /></p>
 OpenStack Admin Password:${adminPassword}
-<p><input type="password" name="adminPassword"/></p>
+<p><input type="password" id="adminPassword"/></p>
 ActiveMQ URL:${mqURL}
-<p><input type="text" name="mqURL"/></p>
-<p><button id="setInfo">设置</button></p>
+<p><input type="text" id="mqURL"/></p>
+<button id="setInfo" onclick="setInfo()">设置</button>
 </div>
-<p>如果您点击我，我会消失。</p>
-<p>点击我，我会消失。</p>
-<p>也要点击我哦。</p>
-
-
-<script>
-document.write("<h1>This is a heading</h2>");
-</script>
 </body>
 </html>
